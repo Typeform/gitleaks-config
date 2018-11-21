6aa588c0ce03d39a729d1375f43caab3205bceb7
Cristhian Amaya <cristhian.amaya@typeform.com>
2018-11-21 11:33:24 +0100

    Update config generator
    When the .secretsignore file does not exists, the generator prints the
    global_config information.

--- gitleaks_config_generator.py
+++ gitleaks_config_generator.py
@@ -9,17 +9,28 @@ the two config files to create the final one that will be passed to gitleaks.
 
 import sys
 import copy
+from pathlib import Path
+
 import toml
 
 
 def main():
-    final_config = merge_config('global_config.toml', '.secretsignore')
+    final_config = get_final_config('global_config.toml', '.secretsignore')
     print(toml.dumps(final_config))
 
 
-def merge_config(global_config_path, repo_config_path):
+def get_final_config(global_config_path, secretsignore_path):
+    if secretsignore_path != '' and Path(secretsignore_path).exists():
+        final_config = merge_config(global_config_path, secretsignore_path)
+        return final_config
+    else:
+        global_config = open_toml(global_config_path)
+        return global_config
+
+
+def merge_config(global_config_path, secretsignore_path):
     global_config = open_toml(global_config_path)
-    repo_config = open_toml(repo_config_path)
+    repo_config = open_toml(secretsignore_path)
     final_config = copy.deepcopy(global_config)
 
     for section, values in repo_config["whitelist"].items():
--- gitleaks_config_generator_tests.py
+++ gitleaks_config_generator_tests.py
@@ -1,10 +1,20 @@
 import unittest
-from gitleaks_config_generator import merge_config
+import gitleaks_config_generator as c
+
 
 class TestGitleaksConfigGenerator(unittest.TestCase):
 
+    def test_get_final_config_without_secretsignore(self):
+        final_config = c.get_final_config('global_config.toml', '')
+        self.assertFalse('*.mp3' in final_config['whitelist']['files'])
+
+    def test_get_final_config_with_secretsignore(self):
+        final_config = c.get_final_config('global_config.toml',
+                                          '.secretsignore')
+        self.assertTrue('*.mp3' in final_config['whitelist']['files'])
+
     def test_merge_config(self):
-        final_config = merge_config('global_config.toml', '.secretsignore')
+        final_config = c.merge_config('global_config.toml', '.secretsignore')
         self.assertTrue('*.mp3' in final_config['whitelist']['files'])
 
 
