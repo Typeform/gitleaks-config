9a45a129093c864c3b2ed6ec3e58737b6890fa4a
David Salvador <david.salvador@typeform.com>
2020-10-19 14:13:01 +0200

    feat: printing gitleaks version and info depending on gitleaks outcome

--- travis_check_secrets.sh
+++ travis_check_secrets.sh
@@ -27,13 +27,29 @@ fi
 # in the before_install step of Travis
 docker pull $gitleaks_container:latest
 
+# Print gitleaks version
+gitleaks_version=$(docker container run --rm --name=gitleaks $gitleaks_container --version)
+echo "Starting secrets scan with gitleaks ${gitleaks_version}"
+
 # Look for secrets in the PR
 docker container run --rm --name=gitleaks -v $PWD/$final_config:/tmp/$final_config \
     $gitleaks_container --host=Github --pr=https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST \
                         --access-token=$GITLEAKS_GITHUB_ACCESS_TOKEN \
                         --config=/tmp/$final_config --verbose --redact
 
-# Autoremove this script when it finishes and maintain the exit code of the
-# gitleaks run
+# Maintain the exit code of the gitleaks run
 exit_code=$?
+
+# If a secret was detected show what to do next
+notion_page='https://www.notion.so/typeform/Detecting-Secrets-and-Keeping-Them-Secret-c2c427bf1ded4b908ce9b2746ffcde88'
+
+if [ $exit_code -eq 0 ]; then
+    echo "Scan finished. No secrets were detected"
+elif [ $exit_code -eq 1 ]; then
+    echo "Scan finished. Looks like one or more secrets were uploaded, check out this Notion page to know what to do next ${notion_page}"
+else
+    echo "Error scanning"
+fi
+
+# Autoremove this script when it finishes
 trap "rm -f $0; exit $exit_code;" EXIT
