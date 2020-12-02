035d616056d1bd5e2763228984d60d621161ba3e
David Salvador <62070820+davidsalvador-tf@users.noreply.github.com>
2020-12-02 14:45:28 +0100

    feat: [SP-840] split hardcoded creds rule (#12)
    * feat: adding one rule per language
    
    * chore: following an allowlist approach in dockerignore
    
    * test: adding more test files
    
    * chore: updating readme
    
    * Update README.md
    
    Co-authored-by: Stanislav Petrov <s.e.petrov@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Stanislav Petrov <s.e.petrov@gmail.com>
    
    * test: fixing file name typo
    
    Co-authored-by: Stanislav Petrov <s.e.petrov@gmail.com>

---
+++ test_data/secrets/variable-assignment.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	password := "5FkbcWgU"
+	pwd := []byte("5FkbcWgU")
+	var secret = `5FkbcWgU`
+
+	fmt.Println(password)
+	fmt.Println(pwd)
+	fmt.Println(secret)
+}
