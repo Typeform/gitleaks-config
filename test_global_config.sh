bd7a30e0d43cd5cb77841baa13b08fa6dcd57d95
Stanislav Petrov <s.e.petrov@gmail.com>
2021-03-17 12:31:40 +0200

    Cleanup after tests

--- test_global_config.sh
+++ test_global_config.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 set -e
+
+# Generate configuration
+final_config="test_gitleaks_config.toml"
+gitleaks_config_container="quay.io/typeform/gitleaks-config"
+repo_dir="${PWD}/test_repo"
+
+cleanup () {
+    rm -f ${final_config} ${repo_dir}
+}
+
+trap cleanup SIGINT
+
 # Run gitleaks on each file of the given directory $1
 # $2 is the value of the expected exit code of gitleaks execution (i.e. secrets detection expected or not)
 # $3 is the error message to be shown when gitleaks' exit code is different than expected
 run_tests () {
     for f in ${1}/*; do
         # Create a new empty repo for each test file
-        repo_dir="${PWD}/test_repo"
-        mkdir ${repo_dir} && cd ${repo_dir} && git init && cd ..
+        mkdir -p ${repo_dir} && cd ${repo_dir} && git init && cd ..
 
         # Copy and git commit the test file
         cp -r ${f} ${repo_dir}
@@ -23,7 +34,6 @@ run_tests () {
             tests_failed=1
         fi
 
-        # Remove the git repo
         rm -rf ${repo_dir}
     done
 }
@@ -36,10 +46,6 @@ run_gitleaks () {
         quay.io/typeform/gitleaks --config=/tmp/gitleaks_config.toml --repo=/tmp/repo --verbose"
     $run_gitleaks
 }
-
-# Generate configuration
-final_config="test_gitleaks_config.toml"
-gitleaks_config_container="quay.io/typeform/gitleaks-config"
 
 docker container run --rm $gitleaks_config_container \
     python gitleaks_config_generator.py > $final_config
@@ -54,13 +60,13 @@ run_tests ${code_with_secrets_dir} 1 "Expecting to detect secrets in"
 code_with_no_secrets_dir="${PWD}/test_data/no_secrets"
 run_tests ${code_with_no_secrets_dir} 0 "Expecting to not detect secrets in"
 
-# Clean up
-rm ${final_config}
-
 if [ ${tests_failed} -eq 0 ]; then
     echo "\033[0;32mTests passed\033[0m"
 else
     echo "\033[0;31mTests failed\033[0m"
 fi
+
+# Cleanup
+cleanup
 
 exit ${tests_failed}
